1ce7c0500bbc3ae85f838a32d37a7bc518fb3689
nguyenduc <1409.ducnguyen@gmail.com>
2025-01-31 17:47:52 +0700

    init code

---
+++ src/main/java/com/nguyenduc/ghidiem/GhidiemApplication.java
@@ -0,0 +1,13 @@
+package com.nguyenduc.ghidiem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GhidiemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GhidiemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nguyenduc/ghidiem/GhidiemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nguyenduc.ghidiem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhidiemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
